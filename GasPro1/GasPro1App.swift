ab6e679e0037b36d28edf1a3be72d0bbfa7e95a3
Shawn Tucker <shqtucke@gmail.com>
2024-09-30 19:40:13 -0400

    Initial Commit

---
+++ GasPro1/GasPro1App.swift
@@ -0,0 +1,17 @@
+//
+//  GasPro1App.swift
+//  GasPro1
+//
+//  Created by Shawn Tucker on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GasPro1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GasPro1Tests/GasPro1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  GasPro1Tests.swift
+//  GasPro1Tests
+//
+//  Created by Shawn Tucker on 9/30/24.
+//
+
+import Testing
+
+struct GasPro1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
